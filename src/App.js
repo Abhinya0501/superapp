c8a7ebaaca2b8515e0189a106f5f42cd797a706f
Abhishek <singhrajputabhishek1234@gmail.com>
2023-08-17 03:33:36 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-import HomePage from "./pages/HomePage.jsx";
-import Form from "./Components/Form.jsx";
+import HomePage from "./pages/HomePage";
+import SelectCategory from "./pages/SelectCategory";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/selectcategory" element={<SelectCategory />} />
+      </Routes>
+    </BrowserRouter>
+    // <HomePage />
   );
 }
 
---
+++ src/pages/SelectCategory.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "../pages/SelectCategory.css";
+export default function SelectCategory() {
+  return <></>;
+}
